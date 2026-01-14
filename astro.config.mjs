18c665ad819d430d5d59e781d311e45a6308bc02
lashapor <lashapor@gmail.com>
2026-01-15 01:30:24 +0400

    refactor: remove GitHub Pages deployment workflow and update site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Lashapor.github.io',
-  base: '/my-portfolio',
+  site: 'https://your-site.vercel.app',
   integrations: [mdx(), sitemap(), tailwind()]
 });
